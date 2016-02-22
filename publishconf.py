678191b65f6678a04c8bea189c92542718e88df3
Guilherme Toti <guilhermetoti@gmail.com>
2016-02-22 13:50:37 -0300

    change page url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://www.guilhermetoti.com.br'
+SITEURL = 'http://www.guilhermetoti.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
