dc5aff15a527546ce41a9668fda078f378a4f2af
Guilherme Toti <guilhermetoti@gmail.com>
2016-02-22 21:18:03 -0300

    change analytics id

--- publishconf.py
+++ publishconf.py
@@ -21,4 +21,4 @@ DELETE_OUTPUT_DIRECTORY = True
 # Following items are often useful when publishing
 
 DISQUS_SITENAME = "guilhermetoti"
-GOOGLE_ANALYTICS = "UA-39972528-10"
+GOOGLE_ANALYTICS = "UA-39972528-7"
